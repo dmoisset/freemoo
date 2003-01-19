a7d408fb6760d23be2f224afaf407c03d49d96fb
dmoisset <dmoisset>
2003-01-19 16:31:27 +0000

    Added tool for converting gimp files to png. Useful to autogenerate data.

---
+++ tools/xcf2png/xcf2png.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+
+#
+# This GimpPerl scripts loads an .xcf file (Gimp format), merges its layers,
+# And saves it as .png with low compression
+#
+
+use Gimp qw(:auto __ N_);
+use Gimp::Fu;
+
+sub xcf2png {
+    my ($filein, $fileout) = @_;
+    $img = gimp_xcf_load (1, $filein, $filein);
+    my @l = $img->get_layers () ;
+    if ($#l>0) {
+        # merge needs two layers, stupid gimp.
+        gimp_layer_new ($img, $img->width, $img->height, 0, "stupid_gimp", 0, 0);
+    }
+    $img->merge_visible_layers (CLIP_TO_IMAGE);
+    $layer = $img->get_active_layer();
+    file_png_save (1, $layer, $fileout, $fileout, 0, 1, 1, 0, 0, 1, 1);
+}
+
+
+register
+  "xcf2png",
+  "desc",
+  "help",
+  "author", "(c)", "date",
+  N_"<Toolbox>/Xtns/Render/xcf2png...",
+  undef,
+  [
+   [PF_STRING, "filein", "", ""],
+   [PF_STRING, "fileout", "", ""]
+  ],
+  \&xcf2png;
+
+exit main;
