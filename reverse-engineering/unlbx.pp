0cac0419944f935948b539c51ce8be4ad874d319
dmoisset <dmoisset>
2003-01-31 17:50:44 +0000

    Typing bugfix in unlbx.
    Updated some .cvsignore files.
    
    Added a palette tool.
    
    Added an automated scriptable extraction tool for ripping MOO2 data.

--- reverse-engineering/unlbx.pp
+++ reverse-engineering/unlbx.pp
@@ -7,31 +7,52 @@ const
 var
    inf,       { Input file }
    ouf: file; { Output file }
-   fc,             { File Count }
+   fc: Word;             { File Count }
    pos, len: Longint; { Position and lengthof File }
    fn: String; { Output filename }
 
-   i, r: Word;
+   i: Word;
    
+   first, last, which, error: Integer;
    buf: Array[1..bufsize] of byte;
    
 Procedure ShowUsage;
 { Show program usage and exit }
 Begin
-   WriteLn ('UnLBX 1.0 - (C) 2001 by Daniel F Moisset under the GPL');
+   WriteLn ('UnLBX 1.1 - (C) 2001 by Daniel F Moisset under the GPL');
    WriteLn;
-   WriteLn ('Usage: unlbx filename');
+   WriteLn ('Usage: unlbx filename [index [output]]');
    Halt (1)
 End;
 
 Begin
    If ParamCount < 1 then ShowUsage; 
+   If ParamCount >= 2 then begin
+       Val (ParamStr (2), which, error) ;
+       if error<>0 then begin
+           WriteLn ('Invalid number: ',ParamStr(2));
+           ShowUsage
+       end;
+   end;
+
    Assign (inf, ParamStr(1));
    Reset (inf, 1);
    
    { Get file count }
    BlockRead (inf, fc, sizeof(fc) );
-   for i := 1 to fc do
+   If ParamCount <2 then begin
+       first := 1 ;
+       last := fc ;
+   end else begin
+       first := which;
+       last := which;
+       if (which<1) or (which>fc) then begin
+           WriteLn ('Index out of bounds: ', which, ' (max ',fc,')');
+           ShowUsage;
+       end;
+   end;
+   
+   for i := first to last do
    Begin
       Seek (inf, 4+4*i) ;
       BlockRead (inf, pos, sizeof(pos) );
@@ -39,8 +60,11 @@ Begin
       len := len - pos;
       
       { Construct file name }
-      Str (i, fn);
-      while Length(fn)<4 do fn:='0'+fn;
+      If ParamCount < 3 then begin
+          Str (i, fn);
+          while Length(fn)<4 do fn:='0'+fn;
+      end else
+          fn := ParamStr (3) ;
       
       Assign (ouf, fn);
       Rewrite (ouf, 1);
